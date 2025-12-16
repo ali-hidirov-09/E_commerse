be68ce244c291fd2f6721b5d395d660e1ee70098
Khidirov Ali <alihidirov285@gmail.com>
2025-12-16 17:11:52 +0600

    yana sozlandi

--- products/models.py
+++ products/models.py
@@ -9,6 +9,7 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
