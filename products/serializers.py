16c8da502216714af55b0896061b44fe7eff6b3e
Khidirov Ali <alihidirov285@gmail.com>
2025-12-16 16:43:12 +0600

    sozlandi

--- products/serializers.py
+++ products/serializers.py
@@ -16,10 +16,10 @@ class ReviewSerializers(serializers.ModelSerializer):
 
 class ProductSerializers(serializers.ModelSerializer):
     avg_rating = serializers.FloatField(read_only=True, required=False)
+
     class Meta:
         model = Product
-        fields = ["id", "name","category","price", "description", "avg_rating"]
-
+        fields = ["id", "name", "category", "price", "description", "avg_rating"]
 
 
 class ProductViewHistorySerializer(serializers.ModelSerializer):
