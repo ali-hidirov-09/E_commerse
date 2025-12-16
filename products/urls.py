c1224bb83962569288919aced89d5e6a03144cf2
Khidirov Ali <alihidirov285@gmail.com>
2025-12-16 19:16:57 +0600

    yana sozlandi

--- products/urls.py
+++ products/urls.py
@@ -4,10 +4,12 @@ from rest_framework.routers import DefaultRouter
 from .services.flash_sale import FlashSaleListCreateView,check_flash_sale
 from .services.product_view_history import ProductViewHistoryCreate
 
+
 router = DefaultRouter()
 router.register(r"products",ProductViewSet )
 router.register(r"reviews", ReviewViewSet)
 router.register(r"categories", CategoryViewSet)
+
 
 urlpatterns = [
     path('', include(router.urls)),
