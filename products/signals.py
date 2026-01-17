0e8be7d62077fd0f0d9619f90221c613703b4c7f
Khidirov Ali <alihidirov285@gmail.com>
2026-01-17 20:29:04 +0600

    tg botdan xabar o'zgartirildi

--- products/signals.py
+++ products/signals.py
@@ -2,9 +2,13 @@ from .models import Order
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .tasks import send_telegram_notifications
+import sys
 
 @receiver(post_save, sender=Order)
 def notify_admin(sender, instance, created, **kwargs):
+    if 'test' in sys.argv:
+        return
+
     if created:
         send_telegram_notifications.delay(
             order_id=instance.id,
--- products/tasks.py
+++ products/tasks.py
@@ -9,8 +9,11 @@ def send_telegram_notifications(order_id, product_name, customer_username, phone
     time.sleep(5)
     token = settings.TELEGRAM_BOT_TOKEN
     method =  'sendMessage'
-    message_text = (f'New order: {order_id}\nProduct: {product_name}\nQuantity: {quantity}\n'
-                    f'Client: {customer_username}\ntel: {phone_number}')
+    message_text = (f'New order: {order_id}\n'
+                    f'Product: {product_name}\n'
+                    f'Quantity: {quantity}\n'
+                    f'Client: {customer_username}\n'
+                    f'tel: {phone_number}')
     response = requests.post(
         url=f"https://api.telegram.org/bot{token}/{method}",
         data={'chat_id': os.environ.get('ADMIN_ID'), 'text':message_text}
