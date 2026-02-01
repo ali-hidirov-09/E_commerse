6a0e268b78ddc46688eecc1677bcfa385c712ad7
Khidirov Ali <alihidirov285@gmail.com>
2026-02-01 08:40:51 +0600

    ortga qaytarildi

--- config/settings.py
+++ config/settings.py
@@ -16,40 +16,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-
-
-#############################################
-import os
-import dj_database_url
-
-# ALLOWED_HOSTS
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
-
-# Database - Railway uchun to'g'ri variant
-if 'DATABASE_URL' in os.environ:
-    # Railway environment
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=os.environ.get('DATABASE_URL'),
-            conn_max_age=600,
-            conn_health_checks=True,
-        )
-    }
-else:
-    # Local environment
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ.get('DB_NAME'),
-            'USER': os.environ.get('DB_ADMIN_USER'),
-            'PASSWORD': os.environ.get('DB_ADMIN_PASSWORD'),
-            'HOST': 'db' if os.environ.get('IS_DOCKER') == 'True' else 'localhost',
-            'PORT': '5432',
-        }
-    }
-
-##################################
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -63,7 +29,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-
+# ALLOWED_HOSTS = ['*']
+if DEBUG:
+    ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,16 +87,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 IS_DOCKER = os.environ.get('IS_DOCKER', 'False') == 'True'
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ.get('DB_NAME'),
-#         'USER': os.environ.get('DB_ADMIN_USER'),
-#         'PASSWORD': os.environ.get('DB_ADMIN_PASSWORD'),
-#         'HOST': 'db' if IS_DOCKER else os.environ.get('DB_HOST','localhost'),
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_ADMIN_USER'),
+        'PASSWORD': os.environ.get('DB_ADMIN_PASSWORD'),
+        'HOST': 'db' if IS_DOCKER else os.environ.get('DB_HOST','localhost'),
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
