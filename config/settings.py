cd632f5d920ecad5180f1c759e4c735a65a7ea1e
Khidirov Ali <alihidirov285@gmail.com>
2026-05-05 13:17:05 +0500

    ortga qaytarildi

--- config/settings.py
+++ config/settings.py
@@ -149,7 +149,7 @@ SIMPLE_JWT = {
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20,
+    'PAGE_SIZE': 5,
 
     'DEFAULT_AUTHENTICATION_CLASSES':[
         'rest_framework_simplejwt.authentication.JWTAuthentication'
